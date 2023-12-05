8cccd388a719de0a71c8c3794aeaf8fdc2d082e4
dmswl6310 <dmswl6310@gmail.com>
2023-12-05 15:15:04 +0900

    feat(OutputVIew): 결과 문구 출략

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -2,7 +2,7 @@ package menu.view;
 
 public class OutputView {
     public void printStartManager() {
-        System.out.println("점심 메뉴 추천을 시작합니다.");
+        System.out.println("점심 메뉴 추천을 시작합니다.\n");
     }
 
     public void printEndManager() {
@@ -15,5 +15,10 @@ public class OutputView {
 
     public void requestHateFood(String name) {
         System.out.printf("%s가 못 먹는 메뉴를 입력해 주세요.\n", name);
+    }
+
+    public void printSelectionResult(String days, String categorys, String selectionResult) {
+        System.out.println("메뉴 추천 결과입니다.");
+        System.out.println(days + categorys + selectionResult);
     }
 }
