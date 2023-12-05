ce23a64992cb0cad608828c14fdfaa186e92250e
dmswl6310 <dmswl6310@gmail.com>
2023-12-05 11:12:59 +0900

    feat(OutputView): 코치 이름 입력 요청 문구 출력

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -8,4 +8,8 @@ public class OutputView {
     public void printEndManager() {
         System.out.println("추천을 완료했습니다.");
     }
+
+    public void requestCoachName() {
+        System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
+    }
 }
