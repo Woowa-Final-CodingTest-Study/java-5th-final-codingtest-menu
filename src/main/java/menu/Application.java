3c82bf15fc06180cb28a8c551b7a5f6e5fd7d683
dmswl6310 <dmswl6310@gmail.com>
2023-12-05 10:19:58 +0900

    feat(OutputView,controller): 프로그램 시작과 문구 출력

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,10 @@
 package menu;
 
+import menu.controller.MenuSelectManager;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        MenuSelectManager menuSelectManager = new MenuSelectManager();
+        menuSelectManager.run();
     }
 }
---
+++ src/main/java/menu/controller/MenuSelectManager.java
@@ -0,0 +1,22 @@
+package menu.controller;
+
+import menu.view.InputView;
+import menu.view.OutputView;
+
+public class MenuSelectManager {
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+
+    public void run() {
+        startManager();
+
+        endManager();
+    }
+
+    private void startManager() {
+        outputView.printStartManager();
+    }
+
+    private void endManager() {
+    }
+}
