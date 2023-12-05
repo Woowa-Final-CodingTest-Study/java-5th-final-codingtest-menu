30ee932f5751d3773c1624e7ed79feb8b1117958
dmswl6310 <dmswl6310@gmail.com>
2023-12-05 11:14:21 +0900

    feat(InputView): 코치이름 입력과 예외처리
    아래 네가지에 대한 예외 추가
    1. 코치가 2명 미만이거나, 5명 초과일때
    2. 각 이름이 2글자 미만이거나, 4글자 초과일때
    3. 각 이름이 한글 또는 영어가 아닌경우(ex.숫자, 특수문자)
    4. 중복된 이름이 있을 때

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,5 +1,71 @@
 package menu.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class InputView {
-    
+    public List<String> readCoachName() {
+        try {
+            List<String> names = Arrays.asList(Console.readLine().split(","));
+            validateNumberOfCoach(names.size());
+            validateCoachName(names);
+            return names;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readCoachName();
+        }
+    }
+
+    void validateNumberOfCoach(int numberOfCoach) {
+        validateMinCoachNumber(numberOfCoach);
+        validateMaxCoachNumber(numberOfCoach);
+    }
+
+    void validateMinCoachNumber(int numberOfCoach) {
+        if (numberOfCoach < 2) {
+            throw new IllegalArgumentException("[ERROR] 코치는 최소 2명 이상 입력해야 합니다.");
+        }
+    }
+
+    void validateMaxCoachNumber(int numberOfCoach) {
+        if (numberOfCoach > 5) {
+            throw new IllegalArgumentException("[ERROR] 코치는 최대 5명 이하로 입력해야 합니다.");
+        }
+    }
+
+    void validateCoachName(List<String> names) {
+        validateDuplicateName(names);
+
+        for (String name : names) {
+            validateNameFormat(name);
+            validateMinNameLength(name);
+            validateMaxNameLength(name);
+        }
+    }
+
+    void validateDuplicateName(List<String> names) {
+        if (names.size() != names.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 중복된 이름이 있습니다.");
+        }
+    }
+
+    void validateNameFormat(String name) {
+        if (!Pattern.matches("^[가-힣a-zA-Z]+$", name)) {
+            throw new IllegalArgumentException("[ERROR] 이름은 한글 또는 영어만 써야합나다.");
+        }
+    }
+
+    void validateMinNameLength(String name) {
+        if (name.length() < 2) {
+            throw new IllegalArgumentException("[ERROR] 이름은 2글자 이상이어야합니다");
+        }
+    }
+
+    void validateMaxNameLength(String name) {
+        if (name.length() < 4) {
+            throw new IllegalArgumentException("[ERROR] 이름은 4글자 이하이어야합니다");
+        }
+    }
 }
