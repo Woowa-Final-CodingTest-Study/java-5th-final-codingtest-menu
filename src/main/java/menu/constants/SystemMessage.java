1eea87de121da5ebe49e274a68b79f10bdbf6fe8
dmswl6310 <dmswl6310@gmail.com>
2023-12-06 17:04:11 +0900

    refactor(controller): 코드 정리

--- src/main/java/menu/constants/SystemMessage.java
+++ src/main/java/menu/constants/SystemMessage.java
@@ -4,6 +4,6 @@ public class SystemMessage {
     public static final String START_SELECTION = "점심 메뉴 추천을 시작합니다.";
     public static final String END_SELECTION = "추천을 완료했습니다.";
     public static final String REQUEST_COACH_NAME = "코치의 이름을 입력해 주세요. (, 로 구분)";
-    public static final String REQUEST_HATE_FOOD = "%s((이)가 못 먹는 메뉴를 입력해 주세요.";
+    public static final String REQUEST_HATE_FOOD = "%s(이)가 못 먹는 메뉴를 입력해 주세요.";
     public static final String SHOW_SELECTION_RESULT = "메뉴 추천 결과입니다.";
 }
